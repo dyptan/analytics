6736afa2f165d73b601df1743d7cbb0662f4a839
dyptan <ivdyptan@gmail.com>
2022-05-10 17:40:30 +0200

    default model

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ mvn clean package -DskipTests -pl $module
 
 docker build --rm -f $module/Dockerfile -t diptan/$module:latest $module
 
-docker push diptan/$module
+#docker push diptan/$module
 done
 # docker run -p 8081:8081 --name trainer diptan/trainer 
 # docker run -p 8080:8080 --name web diptan/web 

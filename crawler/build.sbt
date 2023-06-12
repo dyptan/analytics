da1299fe6eade10d54c125683bbc36d087bc1d6e
dyptan <ivdyptan@gmail.com>
2023-06-12 17:59:30 +0200

    added web server

--- crawler/build.sbt
+++ crawler/build.sbt
@@ -8,8 +8,6 @@ lazy val root = (project in file("."))
     idePackagePrefix := Some("com.dyptan.crawler")
   )
 
-//libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % "3.8.15"
-
 libraryDependencies ++= List(
   "com.softwaremill.sttp.client3" %% "zio" % "3.8.15",
   "com.softwaremill.sttp.client3" %% "circe" % "3.8.15",
@@ -18,10 +16,11 @@ libraryDependencies ++= List(
 
 libraryDependencies ++= Seq(
   "dev.zio" %% "zio-kafka"   % "2.3.1",
+  "dev.zio" %% "zio-http" % "3.0.0-RC2",
+  "dev.zio" %% "zio-akka-cluster" % "0.3.0"
 )
-libraryDependencies += "dev.zio" %% "zio-akka-cluster" % "0.3.0"
-val circeVersion = "0.14.3"
 
+val circeVersion = "0.14.3"
 libraryDependencies ++= Seq(
   "io.circe" %% "circe-core",
   "io.circe" %% "circe-generic",
--- crawler/src/main/scala/Main.scala
+++ crawler/src/main/scala/Main.scala
@@ -3,12 +3,14 @@ package com.dyptan.crawler
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import io.circe.generic.auto._
+import io.circe.parser.decode
 import org.apache.kafka.common.Uuid
 import sttp.client3._
-import sttp.model._
 import sttp.client3.circe._
 import sttp.client3.httpclient.zio._
+import sttp.model._
 import zio.akka.cluster.pubsub.PubSub
+import zio.http.{Method, Request, Response, _}
 import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
 import zio.kafka.producer.{Producer, ProducerSettings}
 import zio.kafka.serde.Serde
@@ -17,28 +19,28 @@ import zio.{Console, Queue, ZIO, ZIOAppDefault, ZLayer}
 
 object Main extends ZIOAppDefault {
   val infoBase = uri"https://developers.ria.com/auto/info?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl"
-  val searchBase = uri"https://developers.ria.com/auto/info?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl"
-  private val urlWithParameters = searchBase
-    .addParam("api_key", "KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl")
-    .addParam("category_id", "1")
-    uri"&category_id=1&" +
-      uri"bodystyle[0]=3&" +
-      uri"bodystyle[4]=2&" +
-      uri"marka_id[0]=79&" +
-      uri"model_id[0]=0&" +
-      uri"s_yers[0]=2010&" +
-      uri"po_yers[0]=2017&" +
-      uri"marka_id[1]=84&" +
-      uri"model_id[1]=0&" +
-      uri"s_yers[1]=2012&" +
-      uri"po_yers[1]=2016&" +
-      uri"brandOrigin[0]=276&" +
-      uri"brandOrigin[1]=392&" +
-      uri"price_ot=1000&price_do=60000&" +
-      uri"currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
+  val searchBase = uri"https://developers.ria.com/auto/search?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl&category_id=1&bodystyle[0]=3&bodystyle[4]=2&marka_id[0]=79&model_id[0]=0&s_yers[0]=2010&po_yers[0]=2017&marka_id[1]=84&model_id[1]=0&s_yers[1]=2012&po_yers[1]=2016&brandOrigin[0]=276&brandOrigin[1]=392&price_ot=1000&price_do=60000&currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
+  private var urlWithParameters: Uri = _
+//    .addParam("api_key", "KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl")
+//    .addParam("category_id", "1")
+//    uri"&category_id=1&" +
+//      uri"bodystyle[0]=3&" +
+//      uri"bodystyle[4]=2&" +
+//      uri"marka_id[0]=79&" +
+//      uri"model_id[0]=0&" +
+//      uri"s_yers[0]=2010&" +
+//      uri"po_yers[0]=2017&" +
+//      uri"marka_id[1]=84&" +
+//      uri"model_id[1]=0&" +
+//      uri"s_yers[1]=2012&" +
+//      uri"po_yers[1]=2016&" +
+//      uri"brandOrigin[0]=276&" +
+//      uri"brandOrigin[1]=392&" +
+//      uri"price_ot=1000&price_do=60000&" +
+//      uri"currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
 
   private val request = basicRequest
-    .get(urlWithParameters)
+    .get(searchBase)
     .response(asJson[searchRoot])
   case class L2(ids: Array[String], count: Int)
   case class L1(search_result: L2)
@@ -126,8 +128,18 @@ object Main extends ZIOAppDefault {
       _ <- ZIO.debug(s"fetched from API: $data")
     } yield data
   }
+
+  val http: Http[Any, Response, Request, Response] = Http.collectZIO[Request] {
+    case req@Method.POST -> Root / "text" =>
+      req.body.asString.map { input =>
+        val inputMap: Map[String, String] = decode[Map[String, String]](input).getOrElse(Map.empty)
+        urlWithParameters = infoBase.addParams(inputMap)
+        Response.text(urlWithParameters.toString())
+      }.mapError(_ => Response.status(Status.BadRequest))
+  }
   override def run: ZIO[Any, Throwable, Unit] = {
     for {
+      _ <- Server.serve(http).fork
       response <- send(request)
       root <- ZIO.fromEither(response.body)
       ids <- ZIO.succeed(root.result.search_result.ids)
@@ -136,6 +148,7 @@ object Main extends ZIOAppDefault {
     } yield ()
 
   }.provide(HttpClientZioBackend.layer(),
+    Server.defaultWithPort(8092),
     producerLayer,
     consumerLayer,
     actorSystem)

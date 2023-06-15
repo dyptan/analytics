4ceba9ab9fc74ebed641468c76e2d94be3990a05
dyptan <ivdyptan@gmail.com>
2023-06-15 14:41:41 +0200

    add periodic fetches

--- crawler/src/main/scala/Main.scala
+++ crawler/src/main/scala/Main.scala
@@ -15,32 +15,16 @@ import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
 import zio.kafka.producer.{Producer, ProducerSettings}
 import zio.kafka.serde.Serde
 import zio.stream.ZStream
-import zio.{Console, Queue, ZIO, ZIOAppDefault, ZLayer}
+import zio.{Console, Duration, Queue, Schedule, ZIO, ZIOApp, ZIOAppDefault, ZLayer, durationInt}
 
 object Main extends ZIOAppDefault {
   val infoBase = uri"https://developers.ria.com/auto/info?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl"
-  val searchBase = uri"https://developers.ria.com/auto/search?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl&category_id=1&bodystyle[0]=3&bodystyle[4]=2&marka_id[0]=79&model_id[0]=0&s_yers[0]=2010&po_yers[0]=2017&marka_id[1]=84&model_id[1]=0&s_yers[1]=2012&po_yers[1]=2016&brandOrigin[0]=276&brandOrigin[1]=392&price_ot=1000&price_do=60000&currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
-  private var urlWithParameters: Uri = _
-//    .addParam("api_key", "KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl")
-//    .addParam("category_id", "1")
-//    uri"&category_id=1&" +
-//      uri"bodystyle[0]=3&" +
-//      uri"bodystyle[4]=2&" +
-//      uri"marka_id[0]=79&" +
-//      uri"model_id[0]=0&" +
-//      uri"s_yers[0]=2010&" +
-//      uri"po_yers[0]=2017&" +
-//      uri"marka_id[1]=84&" +
-//      uri"model_id[1]=0&" +
-//      uri"s_yers[1]=2012&" +
-//      uri"po_yers[1]=2016&" +
-//      uri"brandOrigin[0]=276&" +
-//      uri"brandOrigin[1]=392&" +
-//      uri"price_ot=1000&price_do=60000&" +
-//      uri"currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
+  val searchBase = uri"https://developers.ria.com/auto/search"
+  val searchDefault = uri"https://developers.ria.com/auto/search?api_key=KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl&category_id=1&bodystyle[0]=3&bodystyle[4]=2&marka_id[0]=79&model_id[0]=0&s_yers[0]=2010&po_yers[0]=2017&marka_id[1]=84&model_id[1]=0&s_yers[1]=2012&po_yers[1]=2016&brandOrigin[0]=276&brandOrigin[1]=392&price_ot=1000&price_do=60000&currency=1&auctionPossible=1&state[0]=1&city[0]=0&state[1]=2&city[1]=0&state[2]=10&city[2]=0&abroad=2&custom=1&auto_options[477]=477&type[0]=1&type[1]=2&type[3]=4&type[7]=8&gearbox[0]=1&gearbox[1]=2&gearbox[2]=3&engineVolumeFrom=1.4&engineVolumeTo=3.2&powerFrom=90&powerTo=250&power_name=1&countpage=50&with_photo=1"
+  private var searchWithParameters = searchBase.addParam("api_key", "KoPvKSBRd5YTGGrePubrcokuqONxzHgFrBW8KHrl")
 
   private val request = basicRequest
-    .get(searchBase)
+    .get(searchWithParameters)
     .response(asJson[searchRoot])
   case class L2(ids: Array[String], count: Int)
   case class L1(search_result: L2)
@@ -120,7 +104,8 @@ object Main extends ZIOAppDefault {
       .aggregateAsync(Consumer.offsetBatches)
       .mapZIO(_.commit)
       .drain
-  private def fetch(id: String) = {
+
+    private def fetch(id: String) = {
     for {
       response <- send(basicRequest.get(infoBase.addParam("auto_id", id)).response(asJson[advRoot]))
       root <- ZIO.fromEither(response.body)
@@ -129,27 +114,83 @@ object Main extends ZIOAppDefault {
     } yield data
   }
 
+  def repeatableRun = {
+
+    for {
+       loop <- for {
+        _ <- ZIO.logInfo("starting fetching")
+        response <- send(request)
+        root <- ZIO.fromEither(response.body)
+      } yield ZIO.succeed(root.result.search_result.ids)
+      res <- loop.repeatN(2)
+    } yield res
+  }
+
+  val stream =
+    ZStream.fromSchedule(Schedule.spaced(1.second) >>> Schedule.succeed(send(request)))
+  override def run: ZIO[Any, Throwable, Unit] = {
+    {
+      for {
+//        adsQ <- process(ids.take(2))
+        pubSub <- PubSub.createPubSub[String]
+        idsQueue <- pubSub.listen("ids")
+        adsQueue <- pubSub.listen("ads")
+
+        loop = for {
+        _ <- ZIO.logInfo ("starting fetching")
+        response <- send (request).debug("response received")
+        root <- ZIO.fromEither (response.body)
+        ids <- ZIO.succeed (root.result.search_result.ids)
+        _ <- ZIO.foreach(ids.take(1)) { id => pubSub.publish("ids", id) }.debug(s"id published")
+        } yield ()
+        _ <- loop.repeat(Schedule.spaced(Duration.fromSeconds(2))).fork
+
+
+        loop = for {
+          id <- idsQueue.take
+          _ <- ZIO.debug(s"fetching id: $id")
+          ad <- fetch(id)
+          _ <- pubSub.publish("ads", ad)
+          _ <- ZIO.debug(s"published to pubsub: $ad")
+        } yield ()
+        _ <- loop.forever.fork
+        _ <- producer(adsQueue).merge(consumer).runDrain
+      } yield ()
+
+    }.provide(
+      HttpClientZioBackend.layer(),
+      producerLayer,
+      consumerLayer,
+      actorSystem)
+  }
+
+}
+
+object HttpServer extends ZIOAppDefault {
+  import Main._
   val http: Http[Any, Response, Request, Response] = Http.collectZIO[Request] {
-    case req@Method.POST -> Root / "text" =>
+    case req@Method.POST -> Root / "test" =>
       req.body.asString.map { input =>
         val inputMap: Map[String, String] = decode[Map[String, String]](input).getOrElse(Map.empty)
-        urlWithParameters = infoBase.addParams(inputMap)
-        Response.text(urlWithParameters.toString())
+        val searchWithParameters = infoBase.addParams(inputMap)
+        Response.text(searchWithParameters.toString())
       }.mapError(_ => Response.status(Status.BadRequest))
+
+    case Method.POST -> Root / "apply" =>
+      ZIO.logInfo("receive post request")
+      Main.run
+      ZIO.succeed(Response.ok)
+
   }
   override def run: ZIO[Any, Throwable, Unit] = {
-    for {
-      _ <- Server.serve(http).fork
-      response <- send(request)
-      root <- ZIO.fromEither(response.body)
-      ids <- ZIO.succeed(root.result.search_result.ids)
-      adsQ <- process(ids.take(2))
-      _ <- producer(adsQ).merge(consumer).runDrain
-    } yield ()
+    {
+      for {
+        _ <- Server.serve(http)
+        _ <- ZIO.logInfo("http server started")
+      } yield ()
+    }.provide(Server.defaultWithPort(8092))
+  }
 
-  }.provide(HttpClientZioBackend.layer(),
-    Server.defaultWithPort(8092),
-    producerLayer,
-    consumerLayer,
-    actorSystem)
 }
+
+object Combined extends ZIOApp.Proxy(Main <> HttpServer)
